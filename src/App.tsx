87aca67bf0c9690d6632160244d0f41a5fad8d14
Bryan Mitchell <bryan.bmf@gmail.com>
2024-07-02 13:50:17 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import AboutMe from './components/AboutMe';
+import ContactMe from './components/ContactMe';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <AboutMe />
+        <Projects />
+        <ContactMe />
+    </div>
+  );
+}
+
+export default App;
