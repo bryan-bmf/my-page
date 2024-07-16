8f0849e1817bcc1247b23c7fd7937f3bc722c014
Bryan Mitchell <bryan.bmf@gmail.com>
2024-07-16 15:01:34 -0400

    basic form

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,18 +1,65 @@
-import { Box } from "@chakra-ui/react";
+import {
+	Box,
+	Center,
+	Flex,
+	HStack,
+	Input,
+	Spacer,
+	Text,
+	Textarea,
+} from "@chakra-ui/react";
 
 const ContactMe = () => {
-    return (
-		<Box id="Contact Me" sx={sx.contact}>
-			<h1>Contact Me</h1>
-		</Box>
+	return (
+		<Center>
+			<Box id="Contact Me" sx={sx.contact}>
+				<h1>Contact Me</h1>
+				<HStack w="inherit">
+					<Flex sx={sx.flex}>
+						<Text mb="2">Name:</Text>
+						<Input
+							placeholder="Enter your full name"
+							type="text"
+							sx={sx.input}
+						/>
+					</Flex>
+					<Spacer />
+					<Flex sx={sx.flex}>
+						<Text mb="2">Email:</Text>
+						<Input
+							placeholder="Enter your email"
+							type="email"
+							sx={sx.input}
+						/>
+					</Flex>
+				</HStack>
+				<Textarea
+					placeholder="Write your message"
+					sx={sx.textArea}
+				/>
+			</Box>
+		</Center>
 	);
 };
 
 const sx = {
 	contact: {
-		// scrollMarginTop: "100px",
-		h: "88vh"
-	}
-}
+		h: "88vh",
+		maxW: "75vw",
+	},
+	input: {
+		w: "20rem",
+		mb: "4",
+	},
+	textArea: {
+		resize: "none",
+		size: "md",
+		w: "665px",
+	},
+	flex: {
+		flexDirection: "column",
+		alignItems: "baseline",
+	},
+};
 
 export default ContactMe;
