17904b1b33c153c2bce71fbe6de1a38e4045bfcd
bryan-bmf <111380447+bryan-bmf@users.noreply.github.com>
2024-07-17 10:36:07 -0400

    footer done (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AboutMe from "./components/AboutMe";
 import ContactMe from "./components/ContactMe";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<AboutMe />
 				<Projects />
 				<ContactMe />
+				<Footer />
 			</Box>
 		</div>
 	);
