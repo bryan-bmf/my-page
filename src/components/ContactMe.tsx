9e1c9c28575d2816a902873c90c2a1326c9c5af0
Bryan Mitchell <bryan.bmf@gmail.com>
2024-07-17 09:34:32 -0400

    changed to simple button

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -2,42 +2,38 @@ import {
 	Box,
 	Button,
 	Center,
-	Flex,
-	HStack,
-	Input,
-	Spacer,
-	Text,
-	Textarea,
+	Heading
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 const ContactMe = () => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const handleChange = (event: any) => {
+		if (event.target.name === "name") {
+			setName(event.target.value);
+		} else if (event.target.name === "email") {
+			setEmail(event.target.value);
+		} else if (event.target.name === "message") {
+			setMessage(event.target.value);
+		}
+	};
+
 	return (
 		<Center>
 			<Box id="Contact Me" sx={sx.contact}>
-				<h1>Contact Me</h1>
-				<HStack>
-					<Flex sx={sx.flex}>
-						<Text mb="2">Name:</Text>
-						<Input
-							placeholder="Enter your full name"
-							type="text"
-							sx={sx.input}
-						/>
-					</Flex>
-					<Spacer />
-					<Flex sx={sx.flex}>
-						<Text mb="2">Email:</Text>
-						<Input
-							placeholder="Enter your email"
-							type="email"
-							sx={sx.input}
-						/>
-					</Flex>
-				</HStack>
-				<Textarea placeholder="Write your message..." sx={sx.textArea} />
-				<Flex direction="row-reverse">
-					<Button>Send Email</Button>
-				</Flex>
+				<Heading sx={sx.header}>Stay in touch!</Heading>
+				<Button>
+						<a
+							href="mailto:bryan.mitchellflores@gmail.com"
+							target="_blank"
+						>
+							Contact Me
+						</a>
+					</Button>
 			</Box>
 		</Center>
 	);
@@ -47,22 +43,11 @@ const sx = {
 	contact: {
 		h: "88vh",
 		maxW: "50vw",
-		w: "100%"
-	},
-	input: {
-		mb: "4",
-	},
-	textArea: {
-		resize: "none",
-		w: "100%",
-		h: "200px",
-		mb: "4",
-	},
-	flex: {
-		flexDirection: "column",
-		alignItems: "baseline",
 		w: "100%",
 	},
+	header: {
+		mb: "20"
+	}
 };
 
 export default ContactMe;
