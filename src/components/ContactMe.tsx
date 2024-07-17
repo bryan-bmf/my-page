c0c53a414c0e2d9a55066347bb64b574e170a7e9
bryan-bmf <111380447+bryan-bmf@users.noreply.github.com>
2024-07-17 09:34:55 -0400

    Contact (#3)
    * basic form
    
    * fields width
    
    * changed to simple button

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,18 +1,53 @@
-import { Box } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Center,
+	Heading
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const ContactMe = () => {
-    return (
-		<Box id="Contact Me" sx={sx.contact}>
-			<h1>Contact Me</h1>
-		</Box>
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const handleChange = (event: any) => {
+		if (event.target.name === "name") {
+			setName(event.target.value);
+		} else if (event.target.name === "email") {
+			setEmail(event.target.value);
+		} else if (event.target.name === "message") {
+			setMessage(event.target.value);
+		}
+	};
+
+	return (
+		<Center>
+			<Box id="Contact Me" sx={sx.contact}>
+				<Heading sx={sx.header}>Stay in touch!</Heading>
+				<Button>
+						<a
+							href="mailto:bryan.mitchellflores@gmail.com"
+							target="_blank"
+						>
+							Contact Me
+						</a>
+					</Button>
+			</Box>
+		</Center>
 	);
 };
 
 const sx = {
 	contact: {
-		// scrollMarginTop: "100px",
-		h: "88vh"
+		h: "88vh",
+		maxW: "50vw",
+		w: "100%",
+	},
+	header: {
+		mb: "20"
 	}
-}
+};
 
 export default ContactMe;
