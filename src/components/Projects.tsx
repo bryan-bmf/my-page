d5da03c46b10dd83e2f36fc3f8b2b052d486aea1
Bryan Mitchell <bryan.bmf@gmail.com>
2024-07-18 16:38:15 -0400

    project cards with seed data

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,33 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center, SimpleGrid } from "@chakra-ui/react";
+import projects from "../seed/projects";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
 	return (
-		<Box id="Projects" sx={sx.projects}>
-			<h1>Projects</h1>
-		</Box>
+		<Center>
+			<Box id="Projects" sx={sx.projects}>
+				<h1>Projects</h1>
+				<SimpleGrid columns={[1, 1, 2, 3]} spacing="60px">
+					{projects.map((project) => (
+						<ProjectCard
+							key={project.title}
+							title={project.title}
+							description={project.description}
+							techStack={project.techStack}
+						/>
+					))}
+				</SimpleGrid>
+			</Box>
+		</Center>
 	);
 };
 
 const sx = {
 	projects: {
-		// scrollMarginTop: "100px",
-		h: "88vh"
-	}
-}
+		// maxH: "88vh",
+		h: "100%",
+		maxW: "88vw",
+	},
+};
 
 export default Projects;
---
+++ src/seed/projects.tsx
@@ -0,0 +1,19 @@
+const projects = [
+	{
+		"title": "Anime List",
+		"description": "anime description",
+		"techStack": ["React", "ChakraUI"]
+	},
+    {
+		"title": "Mi Bellonera",
+		"description": "bellonera description",
+		"techStack": ["React", "ChakraUI"]
+	},
+    {
+		"title": "Clean Plate Club",
+		"description": "CPC description",
+		"techStack": ["React", "ChakraUI"]
+	}
+]
+
+export default projects;
