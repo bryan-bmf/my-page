97a82a07b7f0144ab73c4ae85c2a66f009b0e360
Bryan Mitchell <bryan.bmf@gmail.com>
2024-07-19 11:55:56 -0400

    initial desc draft

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,8 +1,17 @@
-import { Card, CardBody, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import {
+    Card,
+    CardBody,
+    Heading,
+    Image,
+    ListItem,
+    Stack,
+    Text,
+    UnorderedList,
+} from "@chakra-ui/react";
 
 const ProjectCard = ({ title, description, techStack }: Props) => {
 	return (
-		<Card maxW="xs" variant="outline" zIndex="-1" >
+		<Card maxW="xs" variant="outline" zIndex="-1">
 			<CardBody>
 				<Image
 					src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
@@ -11,9 +20,13 @@ const ProjectCard = ({ title, description, techStack }: Props) => {
 				/>
 				<Stack mt="6" spacing="3">
 					<Heading size="md">{title}</Heading>
-					<Text>{description}</Text>
-					<Text color="blue.600" fontSize="2xl">
-						{techStack.map((tech) => <ul><li>{tech}</li></ul>)}
+					<Text textAlign="left">{description}</Text>
+					<Text textAlign="left">
+						{techStack.map((tech) => (
+							<UnorderedList>
+								<ListItem>{tech}</ListItem>
+							</UnorderedList>
+						))}
 					</Text>
 				</Stack>
 			</CardBody>
--- src/seed/projects.tsx
+++ src/seed/projects.tsx
@@ -1,19 +1,26 @@
 const projects = [
 	{
-		"title": "Anime List",
-		"description": "anime description",
-		"techStack": ["React", "ChakraUI"]
+		title: "Anime List",
+		description: `A React App where users can search for anime series and characters. 
+		All series and characters will show information pertaining to them including a video section with a list of the most popular videos found in YouTube.
+		I used React Router to learn how to pass information through query parameters to load the correct information passing from page to page.`,
+		techStack: ["React", "ChakraUI", "Jikan API", "YouTube API", "Firebase"],
 	},
-    {
-		"title": "Mi Bellonera",
-		"description": "bellonera description",
-		"techStack": ["React", "ChakraUI"]
+	{
+		title: "Mi Bellonera",
+		description: `A web app to catalog, visualize, and maintain a list of my vinyl record collection.
+		Vinyl records are stored in a NoSQL database with data pulled from Spotify API and displayed to 
+		allow users to listen to albums on the site and view statistics about the record collection.
+		I used AWS Lambda and API Gateway to create a serverless architecture for the app, and I also used DynamoDB for storing the data.`,
+		techStack: ["React", "TypeScript", "ChakraUI", "AWS API Gateway", "AWS DynamoDB", "AWS Lambda", "Spotify API", "Nivo Graphs"],
 	},
-    {
-		"title": "Clean Plate Club",
-		"description": "CPC description",
-		"techStack": ["React", "ChakraUI"]
-	}
-]
+	{
+		title: "Clean Plate Club",
+		description: `A simple web app to keep my favorite recipes. 
+		Implemented different filters to easily find whatever recipe you want to cook.
+		I used a PostgreSQL database to store my recipes and a Flask backend to gain more experience with other technologies.`,
+		techStack: ["React", "TypeScript", "ChakraUI", "Python", "Flask", "PostgreSQL"],
+	},
+];
 
 export default projects;
